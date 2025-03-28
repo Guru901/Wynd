40739aed21a444171af61fe2353ad98253ee3da0
Guru901 <avinashkaur842@gmail.com>
2025-03-28 20:38:26 +0530

    Add WebSocket server functionality with Tokio and Tungstenite; update dependencies and .gitignore

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,77 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use futures::{SinkExt, StreamExt};
+use tokio::net::{TcpListener, TcpStream};
+use tokio_tungstenite::{accept_async, tungstenite::protocol::Message};
+
+pub struct Wynd {
+    port: u16,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Wynd {
+    pub fn new(port: u16) -> Self {
+        Wynd { port }
     }
+
+    pub fn on_connection<F>(&self, cl: F)
+    where
+        F: Fn(WebSocketConn),
+    {
+    }
+
+    pub async fn listen(&self) -> Result<(), String> {
+        let port = self.port;
+        let listener = TcpListener::bind(format!("0.0.0.0:{port}"))
+            .await
+            .map_err(|e| e.to_string())?;
+
+        println!(
+            "Wynd server started on ws://{}",
+            listener.local_addr().unwrap()
+        );
+
+        while let Ok((stream, _)) = listener.accept().await {
+            tokio::spawn(handle_connection(stream));
+        }
+
+        Ok(())
+    }
+}
+
+async fn handle_connection(stream: TcpStream) {
+    let ws_stream = match accept_async(stream).await {
+        Ok(ws) => ws,
+        Err(e) => {
+            println!("Error during the websocket handshake: {}", e);
+            return;
+        }
+    };
+
+    let (mut sender, mut receiver) = ws_stream.split();
+
+    while let Some(msg) = receiver.next().await {
+        match msg {
+            Ok(Message::Text(text)) => {}
+            Ok(Message::Pong(_)) => (),
+            Ok(Message::Binary(_)) => (),
+            Ok(Message::Ping(_)) => (),
+            Ok(Message::Close(_)) => break,
+            Err(e) => {
+                println!("Error processing message: {}", e);
+                break;
+            }
+        }
+    }
+}
+
+pub struct WebSocketConn {}
+
+impl WebSocketConn {
+    pub fn on_message<F>(&self, cl: F)
+    where
+        F: Fn(WebSocketMessageEvent),
+    {
+    }
+}
+
+pub struct WebSocketMessageEvent {
+    pub data: String,
 }
