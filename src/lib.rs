7abb527014cccc8aa6fc18b6eb90af4de3be2f99
Guru901 <avinashkaur842@gmail.com>
2025-03-29 15:56:05 +0530

    Refactor WebSocket connection handling in Wynd; introduce callback for connection events and improve message processing

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,30 @@
-use futures::{SinkExt, StreamExt};
+use std::{
+    cell::{RefCell, RefMut},
+    rc::Rc,
+};
+
+use futures::{SinkExt, StreamExt, stream::SplitSink};
 use tokio::net::{TcpListener, TcpStream};
-use tokio_tungstenite::{accept_async, tungstenite::protocol::Message};
+use tokio_tungstenite::{
+    WebSocketStream, accept_async,
+    tungstenite::{self, protocol::Message},
+};
 
 pub struct Wynd {
     port: u16,
+    on_connection_cl: fn(RefMut<'_, WebSocketConn>),
 }
 
 impl Wynd {
     pub fn new(port: u16) -> Self {
-        Wynd { port }
+        Wynd {
+            port,
+            on_connection_cl: |_| {},
+        }
     }
 
-    pub fn on_connection<F>(&self, cl: F)
-    where
-        F: Fn(WebSocketConn),
-    {
+    pub fn on_connection(&mut self, cl: fn(RefMut<'_, WebSocketConn>)) {
+        self.on_connection_cl = cl;
     }
 
     pub async fn listen(&self) -> Result<(), String> {
@@ -29,49 +39,78 @@ impl Wynd {
         );
 
         while let Ok((stream, _)) = listener.accept().await {
-            tokio::spawn(handle_connection(stream));
+            let on_connection_cl = self.on_connection_cl;
+            async move {
+                let ws_conn = Rc::new(RefCell::new(WebSocketConn::new()));
+
+                (on_connection_cl)(ws_conn.borrow_mut());
+
+                let ws_stream = match accept_async(stream).await {
+                    Ok(ws) => ws,
+                    Err(e) => {
+                        println!("Error during the websocket handshake: {}", e);
+                        return;
+                    }
+                };
+
+                let (sender, mut receiver) = ws_stream.split();
+
+                ws_conn.borrow_mut().sender = Some(sender);
+
+                while let Some(msg) = receiver.next().await {
+                    match msg {
+                        Ok(Message::Text(text)) => {
+                            let event = WebSocketMessageEvent { data: text };
+                            let mut conn = ws_conn.borrow_mut();
+                            (conn.on_message_cl)(event, conn);
+                        }
+                        Ok(Message::Binary(bin)) => {
+                            println!("Received binary message: {:?}", bin);
+                        }
+                        Ok(Message::Ping(_)) => {}
+                        Ok(Message::Pong(_)) => {}
+                        Ok(Message::Close(_)) => break,
+                        Err(e) => {
+                            println!("Error processing message: {}", e);
+                            break;
+                        }
+                    }
+                }
+            }
+            .await;
         }
 
         Ok(())
     }
 }
 
-async fn handle_connection(stream: TcpStream) {
-    let ws_stream = match accept_async(stream).await {
-        Ok(ws) => ws,
-        Err(e) => {
-            println!("Error during the websocket handshake: {}", e);
-            return;
-        }
-    };
-
-    let (mut sender, mut receiver) = ws_stream.split();
-
-    while let Some(msg) = receiver.next().await {
-        match msg {
-            Ok(Message::Text(text)) => {}
-            Ok(Message::Pong(_)) => (),
-            Ok(Message::Binary(_)) => (),
-            Ok(Message::Ping(_)) => (),
-            Ok(Message::Close(_)) => break,
-            Err(e) => {
-                println!("Error processing message: {}", e);
-                break;
-            }
-        }
-    }
+#[derive(Debug, Clone)]
+pub struct WebSocketConn {
+    on_message_cl: fn(WebSocketMessageEvent, RefMut<'_, WebSocketConn>),
+    sender: Option<SplitSink<WebSocketStream<TcpStream>, Message>>,
 }
-
-pub struct WebSocketConn {}
 
 impl WebSocketConn {
-    pub fn on_message<F>(&self, cl: F)
-    where
-        F: Fn(WebSocketMessageEvent),
-    {
+    fn new() -> Self {
+        WebSocketConn {
+            on_message_cl: |_, _| {},
+            sender: None,
+        }
+    }
+    pub fn on_message(&mut self, cl: fn(WebSocketMessageEvent, RefMut<'_, WebSocketConn>)) {
+        self.on_message_cl = cl;
+    }
+
+    pub async fn send(self, data: &str) {
+        self.sender
+            .unwrap()
+            .send(Message::Text(data.to_string()))
+            .await
+            .unwrap();
     }
 }
 
+#[derive(Debug)]
 pub struct WebSocketMessageEvent {
     pub data: String,
 }
