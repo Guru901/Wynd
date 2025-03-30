a5bb1f5ec1fecbe87ee601a8afdfc0c96dd3b03a
Guru901 <avinashkaur842@gmail.com>
2025-03-30 13:21:36 +0530

    Add examples and documentation for Server methods, including connection handling and server startup. Update .gitignore to exclude tarpaulin report.

--- src/wynd.rs
+++ src/wynd.rs
@@ -15,6 +15,26 @@ pub struct Server {
 }
 
 impl Server {
+    /// Creates a new Wynd server.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use wynd::wynd::Server;
+    ///
+    /// let mut wynd = Server::new(8080);
+    ///
+    /// wynd.on_connection(|mut conn| {
+    ///     println!("Client connected");
+    ///     conn.on_message(|_event, conn| {
+    ///         let conn = conn.clone();
+    ///         tokio::spawn(async move {
+    ///             conn.send("hehe").await;
+    ///         });
+    ///     });
+    ///});
+    /// ```
+
     pub fn new(port: u16) -> Self {
         Server {
             port,
@@ -22,9 +42,52 @@ impl Server {
         }
     }
 
+    /// Sets a callback to be called when a new client connects.
+    ///
+    /// The callback is called with a [`WebSocketConn`].
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use wynd::wynd::Server;
+    ///
+    /// let mut wynd = Server::new(8080);
+    ///
+    /// wynd.on_connection(|mut conn| {
+    ///     println!("Client connected");
+    ///     conn.on_message(|_event, conn| {
+    ///         let conn = conn.clone();
+    ///         tokio::spawn(async move {
+    ///             conn.send("hehe").await;
+    ///         });
+    ///     });
+    ///```
+
     pub fn on_connection(&mut self, cl: fn(MutexGuard<'_, WebSocketConn>)) {
         self.on_connection_cl = cl;
     }
+
+    /// Starts the server.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use wynd::wynd::Server;
+    ///
+    /// let mut wynd = Server::new(8080);
+    ///
+    /// wynd.on_connection(|mut conn| {
+    ///     println!("Client connected");
+    ///     conn.on_message(|_event, conn| {
+    ///         let conn = conn.clone();
+    ///         tokio::spawn(async move {
+    ///             conn.send("hehe").await;
+    ///         });
+    ///     });
+    ///});
+    ///
+    /// wynd.listen().await.unwrap();
+    /// ```
 
     pub async fn listen(&self) -> Result<(), String> {
         let port = self.port;
